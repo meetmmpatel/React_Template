be9c7a6d810364ff86cf94b9e48f565d27d73579
meetmmpatel <meetmmpatel@gmail.com>
2020-01-04 12:49:52 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1 className="display-5 ">This is react sample template</h1>
+			<p className="lead">Please modify and check configuration before use</p>
+		</div>
+	);
+}
+
+export default App;
